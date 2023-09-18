ea9f93485a0cea48103c3fc6a61180ec449bbdfa
Kento <72524097+KentoDecem@users.noreply.github.com>
2023-09-18 19:34:11 +0200

    Create Password_Generator.py

---
+++ Day 5/Password_Generator.py
@@ -0,0 +1,116 @@
+#Password Generator Project
+import random
+import time
+import os
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = 8
+nr_symbols = 4
+nr_numbers = 4
+
+print(bcolors.HEADER + "\033[1mWelcome to the PyPassword Generator!\n" +
+      bcolors.ENDC)
+print('''\033[96m
+    _  __          _        
+   | |/ /___ _ __ | |_ ___  
+   | ' // _ \ '_ \| __/ _ \ 
+   | . \  __/ | | | || (_) |
+by |_|\_\___|_| |_|\__\___/ 
+                         
+\033[0m''')
+time.sleep(0.5)
+
+print("------------------------------------------")
+choice = input(
+    "1. Generate password \033[4mautomatically\033[0m\n2. Generate password \033[4mmanually\033[0m\n3. Generate Wordlist for Brute-Force Attack (underdeveloped)\n------------------------------------------\n\nChoose: "
+)
+
+if choice == "1":
+    print("\nBelow is your password, I hope you like it!")
+
+elif choice == "2":
+    nr_letters = int(input("\nGive the number of \033[94mletters\033[0m: "))
+    nr_symbols = int(input("\nGive the number of \033[94msymbols\033[0m: "))
+    nr_numbers = int(input("\nGive the number of \033[94mnumbers\033[0m: "))
+    print("\nHere is your password⬇️")
+
+elif choice == "3":
+    print("\n\033[96mComing soon...\n")
+    print('''\033[94m
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠚⠉⠀⠀⠉⠑⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠀⠀⠀⠀⠀⠀⠀⠀⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣠⠔⠋⠉⣩⣍⠉⠙⠢⣄⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢧⡜⢏⠓⠒⠚⠁⠈⠑⠒⠚⣹⢳⡸⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠸⡄⠀⠀⠀⠀⠀⠀⢠⠇⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠚⠉⢣⡙⢦⡀⠀⠀⢀⡰⢋⡜⠉⠓⠦⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡴⠁⢀⣀⣀⣀⣙⣦⣉⣉⣋⣉⣴⣋⣀⣀⣀⡀⠈⢧⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⡸⠁⠀⢸⠀⠀⠀⠀⢀⣔⡛⠛⡲⡀⠀⠀⠀⠀⡇⠀⠈⢇⠀⠀⠀⠀
+⠀⠀⠀⢠⠇⠀⠀⠸⡀⠀⠀⠀⠸⣼⠽⠯⢧⠇⠀⠀⠀⠀⡇⠀⠀⠘⡆⠀⠀⠀
+⠀⠀⠀⣸⠀⠀⠀⠀⡇⠀⠀⠀⠳⢼⡦⢴⡯⠞⠀⠀⠀⢰⠀⠀⠀⠀⢧⠀⠀⠀
+⠀⠀⠀⢻⠀⠀⠀⠀⡇⠀⠀⠀⢀⡤⠚⠛⢦⣀⠀⠀⠀⢸⠀⠀⠀⠀⡼⠀⠀⠀
+⠀⠀⠀⠈⠳⠤⠤⣖⣓⣒⣒⣒⣓⣒⣒⣒⣒⣚⣒⣒⣒⣚⣲⠤⠤⠖⠁⠀⠀⠀
+⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+  ''')
+    exit()
+
+else:
+    print("There is no such an option.")
+    exit()
+
+l = ""
+s = ""
+n = ""
+
+
+def displayIntro():
+    global l
+    global s
+    global n
+
+    for letter in range(nr_letters):
+        l += random.choice(letters)
+
+    for symbol in range(nr_symbols):
+        s += random.choice(symbols)
+
+    for number in range(nr_symbols):
+        n += random.choice(numbers)
+
+    combined = l + s + n
+    mixed = list(combined)
+    random.shuffle(mixed)
+    print("\n\033[92m" + bcolors.BOLD + "".join(mixed) + bcolors.ENDC)
+
+
+playagain = 'yes'
+while playagain == 'yes':
+    displayIntro()
+    l = ""
+    s = ""
+    n = ""
+    playagain = input('\nGenerate again ("yes" / "no"): ')
