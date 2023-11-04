3b0bbd6e801074fc290a498b7fb16bd879ff66df
Kento <72524097+KentoDecem@users.noreply.github.com>
2023-11-04 12:41:55 +0100

    Flight Club

---
+++ Day 40/notification_manager.py
@@ -0,0 +1,18 @@
+import smtplib
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+
+class NotificationManager:
+    def send_emails(self, emails, message):
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
+
+
